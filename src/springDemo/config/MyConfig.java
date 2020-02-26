d8865e7138484c212c1ce51a222ecb44805d852d
Risus <gesangcx@outlook.com>
2020-02-26 22:48:51 +0800

    aaa

--- src/springDemo/config/MyConfig.java
+++ src/springDemo/config/MyConfig.java
@@ -4,11 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import springDemo.entity.Adress;
 import springDemo.entity.Student;
 
 @Configuration
-@ComponentScan(value="springDemo")
+@ComponentScan(value="springDemo", excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Service.class, Repository.class})})
 public class MyConfig {
 	
 	@Bean
